14796b6c1d70133643ab7ac187e21af7dad9fba7
iamclement1 <blessedmarcel1@gmail.com>
2023-02-06 10:30:41 +0100

    update: added packages firebase, framer-motion, react-icons, react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Navbar } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-      <div className="text-5xl">Foodie</div>
+      <div className="w-screen h-auto flex flex-col">
+        <Navbar />
+      </div>
     </div>
   )
 }
