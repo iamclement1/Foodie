65b4ea82a8ad363ba4aa9258ac23c392764f28ce
iamclement1 <blessedmarcel1@gmail.com>
2023-02-06 23:19:01 +0100

    feats: navbar + added router in main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='fixed z-50 w-screen bg-slate-600 p-6 px-16 ' >
+            {/* desktop & tablet */}
+            <div className='hidden md:flex w-full bg-red-600 p-3 border-none' ></div>
+
+
+            {/* mobile phone  */}
+            <div className="flex md:hidden w-full h-full bg-blue-500 p-5" ></div>
+        </div>
     )
 }
 
