08d68577bbaffbdb3ddaaeb4906a6a903773a932
iamclement1 <blessedmarcel1@gmail.com>
2023-02-07 19:30:09 +0100

    page routing imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { Navbar } from './components'
+import Routing from './routes/Routing'
 
 function App() {
   
   return (
     <div className="App">
-      <div className="w-screen h-auto flex flex-col">
+      <div className="w-screen h-auto flex flex-col bg-primary">
         <Navbar />
+        <main className='mt-24 p-8 w-full'>
+          <Routing />
+        </main>
       </div>
     </div>
   )
