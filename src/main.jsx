52c56c96ef25313eeb669f7bc1a1305253372088
iamclement1 <blessedmarcel1@gmail.com>
2023-02-08 20:48:14 +0100

    wrap app with stateprovider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { StateProvider } from './context/StateProvider'
+import { initialState } from './context/initialState'
+import reducer from './context/reducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </Router>,
 )
