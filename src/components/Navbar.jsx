9669005b2ac5a6f08e80a16a2c97df391e4c2b4a
iamclement1 <blessedmarcel1@gmail.com>
2023-02-11 16:41:28 +0100

    logout function + menu state hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import Logo from '../assets/img/logo.png';
 import Avatar from '../assets/img/avatar.png';
 import { IoMdCart } from 'react-icons/io';
+import { MdAdd, MdLogout } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config';
 import { useStateValue } from '../context/StateProvider';
 import { actionType } from "../context/reducer";
+import { useState } from 'react';
 
 const Navbar = () => {
 
@@ -14,17 +16,33 @@ const Navbar = () => {
     const provider = new GoogleAuthProvider();
 
     //dispatch providerData by using the useStateValue custome hook I created in the context folder
-    const [ { user }, dispatch ] = useStateValue();
+    const [{ user }, dispatch] = useStateValue();
+
+    const [isMenu, setIsMenu] = useState(false);
 
     const Login = async () => {
-        const { user : { refreshToken, providerData} } = await signInWithPopup(firebaseAuth, provider)
-        //dispatch providerData
+        if (!user) {
+            const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider)
+            //dispatch providerData
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0]
+            });
+            localStorage.setItem('user', JSON.stringify(providerData[0]));
+        } else {
+            setIsMenu(!isMenu)
+        }
+    };
+
+    const Logout = () => {
+        setIsMenu(false);
+        localStorage.removeItem('user');
+
         dispatch({
             type: actionType.SET_USER,
-            user : providerData[0]
-        });
-        localStorage.setItem('user', JSON.stringify(providerData[0]));
-    };
+            user: null,
+        })
+    }
     return (
         <header className='fixed z-50 w-screen p-6 px-16 ' >
             {/* desktop & tablet */}
@@ -68,11 +86,35 @@ const Navbar = () => {
                     {/* avatarImage + framer motion to tapping */}
                     <div className="relative">
                         <motion.img
-                            whileTap={{ scale: 0.6 }} 
-                            src={ user ? user.photoURL : Avatar } //if user is signed in display user image.
-                            alt="user__profile" 
-                            className='w-10 min-w-[40px] h-10 min-h-[40x] drop-shadow-2xl cursor-pointer rounded-full' 
-                            onClick={Login}/>
+                            whileTap={{ scale: 0.6 }}
+                            src={user ? user.photoURL : Avatar} //if user is signed in display user image.
+                            alt="user__profile"
+                            className='w-10 min-w-[40px] h-10 min-h-[40x] drop-shadow-2xl cursor-pointer rounded-full'
+                            onClick={Login} />
+
+                        {
+                            isMenu && (
+                                <div className='flex flex-col w-40 bg-primary shadow-xl rounded-lg absolute
+                        top-12 right-0 '>
+                                    {/* render this only if the user is an admin */}
+                                    {
+                                        user && user.email === 'blessedmarcel1@gmail.com' && (
+                                            <Link to={'/createItem'}>
+                                                <p className='flex items-center px-4 py-2 gap-3 cursor-pointer hover:bg-slate-100
+                            transition-all duration-100 ease-in-out text-textColor text-base'>
+                                                    New Item <MdAdd />
+                                                </p>
+                                            </Link>
+                                        )
+                                    }
+                                    <p className='flex items-center px-4 py-2 gap-3 cursor-pointer hover:bg-slate-100
+                            transition-all duration-100 ease-in-out text-textColor text-base'
+                                        onClick={Logout}>
+                                        Logout <MdLogout />
+                                    </p>
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
 
