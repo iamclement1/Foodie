d4629e71a029f5c8bad7556ca9c5799a6f89598f
iamclement1 <blessedmarcel1@gmail.com>
2023-02-11 17:03:39 +0100

    mobile view navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         })
     }
     return (
-        <header className='fixed z-50 w-screen p-6 px-16 ' >
+        <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 ' >
             {/* desktop & tablet */}
             <div className='hidden md:flex w-full h-full items-center justify-between' >
                 <Link to={'/'} className="flex items-center gap-2">
@@ -56,7 +56,11 @@ const Navbar = () => {
 
                 <div className="flex items-center gap-8">
                     {/* navlinks list */}
-                    <ul className='flex items-center gap-6'>
+                    <motion.ul
+                        initial={{ opacity: 0, x: 200 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 200 }}
+                        className='flex items-center gap-6'>
                         <li className="text-lg text-textColor hover:text-headingColor duration-100 
                         transition-all ease-in-out cursor-pointer">
                             Home
@@ -73,7 +77,7 @@ const Navbar = () => {
                         transition-all ease-in-out cursor-pointer">
                             Service
                         </li>
-                    </ul>
+                    </motion.ul>
 
                     {/* cart icon */}
                     <div className="relative flex items-start justify-center">
@@ -95,10 +99,10 @@ const Navbar = () => {
                         {
                             isMenu && (
                                 <motion.div
-                                initial={{ opacity : 0, scale : 0.6 }}
-                                animate={{ opacity : 1, scale : 1 }}
-                                exit={{ opacity : 0, scale : 0.6 }}
-                                className='flex flex-col w-40 bg-primary shadow-xl rounded-lg absolute
+                                    initial={{ opacity: 0, scale: 0.6 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.6 }}
+                                    className='flex flex-col w-40 bg-primary shadow-xl rounded-lg absolute
                         top-12 right-0 '>
                                     {/* render this only if the user is an admin */}
                                     {
@@ -126,7 +130,71 @@ const Navbar = () => {
 
 
             {/* mobile phone  */}
-            <div className="flex md:hidden w-full h-full bg-blue-500 p-5" ></div>
+            <div className="flex items-center justify-between md:hidden w-full h-full p-5" >
+                <Link to={'/'} className="flex items-center gap-2">
+                    <img src={Logo} alt="Logo" className='w-10 object-cover' />
+                    <p className='text-headingColor text-xl font-bold'>
+                        Foodie
+                    </p>
+                </Link>
+
+                {/* avatarImage + framer motion to tapping */}
+                <div className="relative">
+                    <motion.img
+                        whileTap={{ scale: 0.6 }}
+                        src={user ? user.photoURL : Avatar} //if user is signed in display user image.
+                        alt="user__profile"
+                        className='w-10 min-w-[40px] h-10 min-h-[40x] drop-shadow-2xl cursor-pointer rounded-full'
+                        onClick={Login} />
+
+                    {
+                        isMenu && (
+                            <motion.div
+                                initial={{ opacity: 0, scale: 0.6 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.6 }}
+                                className='flex flex-col w-40 bg-primary shadow-xl rounded-lg absolute
+                        top-12 right-0 '>
+                                {/* render this only if the user is an admin */}
+                                {
+                                    user && user.email === 'blessedmarcel1@gmail.com' && (
+                                        <Link to={'/createItem'}>
+                                            <p className='flex items-center px-4 py-2 gap-3 cursor-pointer hover:bg-slate-100
+                            transition-all duration-100 ease-in-out text-textColor text-base'>
+                                                New Item <MdAdd />
+                                            </p>
+                                        </Link>
+                                    )
+                                }
+                                {/* navlinks list */}
+                                <ul
+                                    className='flex flex-col'>
+                                    <li className="text-lg text-textColor hover:text-headingColor duration-100 
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">
+                                        Home
+                                    </li>
+                                    <li className="text-lg text-textColor hover:text-headingColor duration-100 
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">
+                                        Menu
+                                    </li>
+                                    <li className="text-lg text-textColor hover:text-headingColor duration-100 
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">
+                                        About Us
+                                    </li>
+                                    <li className="text-lg text-textColor hover:text-headingColor duration-100 
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">
+                                        Service
+                                    </li>
+                                </ul>
+                                <p className='flex items-center px-4 py-2 gap-3 cursor-pointer bg-slate-500 justify-center hover:bg-slate-100 text-white transition-all duration-100 ease-in-out text-base'
+                                    onClick={Logout}>
+                                    Logout <MdLogout />
+                                </p>
+                            </motion.div>
+                        )
+                    }
+                </div>
+            </div>
         </header>
     )
 }
