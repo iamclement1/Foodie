8e8f1d9d559254b35d711179750e5dc673963a4f
iamclement1 <blessedmarcel1@gmail.com>
2023-02-12 16:48:44 +0100

    fetch hero data

---
+++ src/utils/HeroData.js
@@ -0,0 +1,38 @@
+import Icecream from '../assets/img/i1.png';
+import Fruits from '../assets/img/f1.png';
+import Food from '../assets/img/c3.png';
+import Fish from '../assets/img/fi1.png';
+
+export const heroData = [
+    {
+        id: 1,
+        name: 'Ice Cream',
+        despcription: 'Chocolate & Vanilla',
+        price: '5.25',
+        img: Icecream
+    },
+
+    {
+        id: 2,
+        name: 'Strawberry',
+        despcription: 'Fresh Strawberries',
+        price: '10.25',
+        img: Fruits
+    },
+
+    {
+        id: 3,
+        name: 'Chicken Kebab',
+        despcription: 'Mixed Kebab Plate',
+        price: '5.25',
+        img: Food
+    },
+
+    {
+        id: 4,
+        name: 'Fish',
+        despcription: 'Mixed Fish',
+        price: '5.25',
+        img: Fish
+    },
+]
