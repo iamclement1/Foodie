d9a50efad53884e4fb4410ad5c588ae4cdae7f40
iamclement1 <blessedmarcel1@gmail.com>
2023-02-15 18:26:58 +0100

    feats: categoryData

---
+++ src/utils/CategoryData.js
@@ -0,0 +1,40 @@
+
+export const categoryData = [
+    {
+        id: 1,
+        name: "Chicken",
+        url: "chicken",
+    },
+    {
+        id: 2,
+        name: "Curry",
+        url: "curry",
+    },
+    {
+        id: 3,
+        name: "Rice",
+        url: "rice",
+    },
+    {
+        id: 4,
+        name: "Fish",
+        url: "fish",
+    },
+    {
+        id: 5,
+        name: "Fruits",
+        url: "fruits",
+    },
+    {
+        id: 6,
+        name: "Icecreams",
+        url: "icecreams",
+    },
+
+    {
+        id: 7,
+        name: "Soft Drinks",
+        url: "drinks",
+    },
+
+]
