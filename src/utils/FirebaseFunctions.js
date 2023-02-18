aa4112e704914261a42e9117745e8578c26ddbff
iamclement1 <blessedmarcel1@gmail.com>
2023-02-18 13:39:44 +0100

    "fetch food item data from firestore database"

--- src/utils/FirebaseFunctions.js
+++ src/utils/FirebaseFunctions.js
@@ -1,9 +1,20 @@
 //functons for saving new item
 
-import { doc, setDoc } from "firebase/firestore"
+import { collection, doc, getDocs, orderBy, query, setDoc } from "firebase/firestore"
 import { firestore } from "../firebase.config"
 
 export const saveItem = async (data) => {
     //set doc from firebase/firestore, fooditem is the collection name, date is the id while the data is the data object we saving
     await setDoc(doc(firestore, 'foodItems', `${Date.now()}`), data, {merge : true})
+};
+
+
+//fetch available items from the database
+
+
+export const getAllItems = async () => { 
+    const items = await getDocs( //getDocs is used to get all items from the database
+        query (collection(firestore, "foodItems"), orderBy("id", "desc"))
+    );
+    return items.docs.map((doc) => doc.data());
 };
