e2879e9f2e1c8f1a90dcae9e4a0d353de2159134
iamclement1 <blessedmarcel1@gmail.com>
2023-02-19 10:17:55 +0100

    row container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,37 @@
 import HomeContainer from "./HomeContainer";
+import { motion } from 'framer-motion';
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import RowContainer from "./RowContainer";
+
 
 const MainContainer = () => {
     return (
         <div className='w-full h-auto flex flex-col items-center justify-center'>
             <HomeContainer />
+            <section className="w-full my-6">
+                <div className="w-full flex items-center justify-between">
+                    <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+                        Fresh & healthy fruits
+                    </p>
+                    <div className="hidden md:flex gap-3 items-center ">
+                        <motion.div
+                            whileTap={{ scale: 0.75 }}
+                            className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer flex
+                        items-center justify-center transition-all duration-100 ease-in-out hover:shadow-lg">
+                            <MdChevronLeft className="text-lg text-white" />
+                        </motion.div>
+                        <motion.div
+                            whileTap={{ scale: 0.75 }}
+                            className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer flex
+                        items-center justify-center transition-all duration-100 ease-in-out hover:shadow-lg">
+                            <MdChevronRight className="text-lg text-white" />
+                        </motion.div>
+                    </div>
+                </div>
+
+                {/* set a flag to true it'll ask for food section if false main menu */}
+                <RowContainer flag={true} />
+            </section>
         </div>
     )
 }
