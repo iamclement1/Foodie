1524c96b08cd5ab973f360e8ba4b7714996c9ae7
iamclement1 <blessedmarcel1@gmail.com>
2023-02-20 17:17:16 +0100

    Changed screen height'

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -6,7 +6,7 @@ import { heroData } from '../utils/HeroData';
 const HomeContainer = () => {
 
     return (
-        <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-screen' id='home'>
+        <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-auto' id='home'>
             <div className="py-2 flex-1 flex flex-col items-start justify-center gap-4">
                 <div className='flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full'>
                     <p className='text-base text-orange-500 font-semibold'>Bike Delivery</p>
