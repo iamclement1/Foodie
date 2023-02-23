8abfa17e9f6adbd37d5933bd71d9638b9f5d36b9
iamclement1 <blessedmarcel1@gmail.com>
2023-02-23 19:27:27 +0100

    cartshow reducer

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -2,6 +2,7 @@
 export const actionType = {
     SET_USER: ' SET_USER',
     SET_FOOD_ITEMS: ' SET_FOOD_ITEMS',
+    SET_CART_SHOW: ' SET_CART_SHOW',
 }
 //data layer
 const reducer = (state, action) => {
@@ -20,6 +21,13 @@ const reducer = (state, action) => {
             return {
                 ...state, //spreader operator
                 foodItems: action.foodItems //update foodItems action
+            };
+
+            // CART SHOW STATE
+        case actionType.SET_CART_SHOW:
+            return {
+                ...state, //spreader operator
+                cartShow: action.cartShow //update cartShow action
             };
         default: return state;
     }
