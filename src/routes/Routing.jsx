4a935c2f957f277cf15fc616ac3926bd6b0ccbb7
iamclement1 <blessedmarcel1@gmail.com>
2023-02-28 17:47:13 +0100

    'update: added aboutus routing'

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { CreateContainer, MainContainer } from '../components'
-
+import { About } from '../pages/index'
 const Routing = () => {
     return (
         <>
         <Routes>
             <Route path='/*' element={ <MainContainer />} />
             <Route path='/createItem' element={ <CreateContainer /> } />
+            <Route path='/about' element={ <About /> } />
         </Routes>
         </>
     )
